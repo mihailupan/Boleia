e6784eddcc1c55f1279a91f4f0f7e25d3f501660
LeonilSulude <leonilchaplin@gmail.com>
2021-01-29 02:29:02 +0000

    Colocar dados na base de dados

--- app/src/main/java/com/example/boleia/CreateVehicleActivity.java
+++ app/src/main/java/com/example/boleia/CreateVehicleActivity.java
@@ -1,22 +1,50 @@
 package com.example.boleia;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class CreateVehicleActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
+    public static final int CAMERA_PERM_CODE = 101;
+    public static final int CAMERA_REQUEST_CODE = 102;
+    public static final int GALLERY_REQUEST_CODE = 105;
     //TODO
     String [] data;
-    Button createTravelButton, openGalleryButtonVehicle, captureImageButtonVehicle;
+    Button createTravelButton;
+    ImageButton openGalleryButtonVehicle, captureImageButtonVehicle;
+    EditText editBrandVehicle, editModelVehicle, editLicensePlateVehicle, editSeatNumberVehicle;
+    String currentPhotoPath;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,13 +59,30 @@ public class CreateVehicleActivity extends AppCompatActivity implements BottomNa
         createTravelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-
                 String txtToDB = getData();
                 Toast.makeText(CreateVehicleActivity.this, txtToDB, Toast.LENGTH_LONG).show();
             }
         });
+
+
+        //Button to capture
+        captureImageButtonVehicle = findViewById(R.id.captureImageButtonVehicle);
+        captureImageButtonVehicle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askCameraPermissions();
+            }
+        });
+
+        //Gallery button
+        openGalleryButtonVehicle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, GALLERY_REQUEST_CODE);
+            }
+        });
+
         //FALTA DAR ACAO AO BOTAO
 
         //TODO
@@ -49,6 +94,87 @@ public class CreateVehicleActivity extends AppCompatActivity implements BottomNa
 
     }
 
+    private void askCameraPermissions() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        }
+        else
+        {
+            dispatchTakePictureIntent();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //Has permission to access camera
+        if(requestCode == CAMERA_PERM_CODE)
+        {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+               dispatchTakePictureIntent();
+            }
+            else
+            {
+                Toast.makeText(this, "É necessária ativar a permissão para utilizar a camara!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    //Photo taken
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+           if(resultCode == Activity.RESULT_OK)
+           {
+               File f = new File(currentPhotoPath);
+               //selectedImage.setImageURI(Uri.fromFile(f));
+
+               Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+               Uri contentUri = Uri.fromFile(f);
+               mediaScanIntent.setData(contentUri);
+               this.sendBroadcast(mediaScanIntent);
+           }
+        }
+
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if(resultCode == Activity.RESULT_OK)
+            {
+                Uri contentUri = data.getData();
+                String timeStap = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" + timeStap + " . "+getFileExt(contentUri);
+
+            }
+        }
+
+    }
+
+    private String getFileExt(Uri contentUri) {
+        ContentResolver c = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(c.getType(contentUri));
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES); //Do not save on gallery
+        //File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES); //Save on gallery
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
     private String getData() {
 
         String str = "";
@@ -57,6 +183,31 @@ public class CreateVehicleActivity extends AppCompatActivity implements BottomNa
             str += data[i] +"\n";
         }
         return str;
+    }
+
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                  //TODO Write error message
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.boleia.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
     }
 
     @Override
